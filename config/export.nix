b3845c6fdb1bf4de5eeeb1e5d74d57a6c537edef
Ludvig Källqvist Nygren <ludvigkallqvistnygren@gmail.com>
2024-06-15 05:01:41 +0000

    fix: corrected branch of nvim

--- config/export.nix
+++ config/export.nix
@@ -3,5 +3,7 @@
     ".config/fastfetch/config.jsonc".source = ./fastfetch.jsonc;
     ".config/nvim/init.lua".source = ./nvim/init.lua;
     ".config/nvim/lua".source = ./nvim/lua;
+    ".config/nvim/after".source = ./nvim/after;
+    ".config/nvim/plugin".source = ./nvim/plugin;
   };
 }
