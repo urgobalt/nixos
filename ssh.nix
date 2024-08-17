f4d02eb12ba077b6374939f0d3dc669902f96336
Ludvig Källqvist Nygren <ludvigkallqvistnygren@gmail.com>
2024-08-17 18:09:14 +0200

    fix: new ssh for new computer

--- ssh.nix
+++ ssh.nix
@@ -1,7 +1,7 @@
 let
   wsl = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOSS+sRSnl0fuUJvQSuXjDSuxoLbAeY8MoApbfwGvVGf";
   pi = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGFAqiX2xokcBhY+BDl4yfSqI/M6mOOSgODyINQ2mi7f";
-  lenovo_laptop_320 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ1IDqCKb01HgcUeJGRrKBP6CQjYJYhFxitZZHBzURPM";
+  lenovo_laptop_320 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBQlHvosfQFeQGsV7EzBN39BBhf1Q3RzAE2qyRj5PtyU";
 in {
   inherit wsl pi lenovo_laptop_320;
   users = [wsl lenovo_laptop_320];
