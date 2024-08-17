6beb729bd74e1e0f23a96d7f5d5a64d7b77fd3d0
Ludvig Källqvist Nygren <ludvigkallqvistnygren@gmail.com>
2024-08-17 11:57:37 +0200

    feat: new computer added to secrets

--- ssh.nix
+++ ssh.nix
@@ -1,8 +1,9 @@
 let
   wsl = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOSS+sRSnl0fuUJvQSuXjDSuxoLbAeY8MoApbfwGvVGf";
   pi = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGFAqiX2xokcBhY+BDl4yfSqI/M6mOOSgODyINQ2mi7f";
+  lenovo_laptop_320 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ1IDqCKb01HgcUeJGRrKBP6CQjYJYhFxitZZHBzURPM";
 in {
-  inherit wsl pi;
-  users = [wsl];
+  inherit wsl pi lenovo_laptop_320;
+  users = [wsl lenovo_laptop_320];
   systems = [pi];
 }
