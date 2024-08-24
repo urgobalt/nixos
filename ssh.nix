cdb9298be5d480267c0a20043da6b4cbd9a37bc1
Ludvig Källqvist Nygren <ludvigkallqvistnygren@gmail.com>
2024-08-24 09:42:24 +0200

    fix: ssh naming

--- ssh.nix
+++ ssh.nix
@@ -1,9 +1,9 @@
 let
   wsl = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOSS+sRSnl0fuUJvQSuXjDSuxoLbAeY8MoApbfwGvVGf";
   pi = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGFAqiX2xokcBhY+BDl4yfSqI/M6mOOSgODyINQ2mi7f";
-  lenovo_laptop_320 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBQlHvosfQFeQGsV7EzBN39BBhf1Q3RzAE2qyRj5PtyU";
+  lenovo-laptop-320 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBQlHvosfQFeQGsV7EzBN39BBhf1Q3RzAE2qyRj5PtyU";
 in {
-  inherit wsl pi lenovo_laptop_320;
-  users = [wsl lenovo_laptop_320];
+  inherit wsl pi lenovo-laptop-320;
+  users = [wsl lenovo-laptop-320];
   systems = [pi];
 }
