ce77a3e4bacc3f30dce8ed5b9f32e57a08ed7a96
Ludvig Källqvist Nygren <ludvigkallqvistnygren@gmail.com>
2024-12-12 17:09:23 +0100

    fix: internet

--- hosts/lenovo-laptop-320/system.nix
+++ hosts/lenovo-laptop-320/system.nix
@@ -1,36 +1,5 @@
 let
-  networks = {
-    "Gobalt_link" = {
-      psk = "@PSK_GOBALT_LINK@";
-      priority = 3;
-    };
-    "Spökhuset" = {
-      psk = "@PSK_SPOKHUSET@";
-      priority = 4;
-    };
-    "Nygren" = {
-      psk = "@PSK_NYGREN@";
-      priority = 5;
-    };
-    "Cinderblock" = {
-      psk = "@PSK_CINDERBLOCK@";
-      priority = 10;
-    };
-    "eduroam" = {
-      auth = ''
-        key_mgmt=WPA-EAP
-        eap=PEAP
-        identity="@I_EDUROAM@"
-        password="@PSK_EDUROAM@"
-        phase2="auth=MSCHAPV2"
-      '';
-      priority = 20;
-    };
-    "urgobalt" = {
-      psk = "@PSK_URGOBALT@";
-      priority = 99;
-    };
-  };
+  networks = import ../networks.nix;
 in {
   config.modules = {
     declarative-password.enable = true;
